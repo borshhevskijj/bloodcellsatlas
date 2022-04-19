d5882182a5a27daa73ddcc1c9b37ae5860a4e52f
borshhevskijj <borshhevskijj98@mail.ru>
2022-04-19 13:15:42 +0300

    bugs fixed

--- src/components/CELLS/NotFound.jsx
+++ src/components/CELLS/NotFound.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import cl from './notFound.module.css'
-// import svg from '../../datacells/images/backgroundImg/404page.svg'
 import svg from '../../datacells/images/backgroundImg/404page01.svg'
-// import svg from '../../datacells/images/backgroundImg/erythrocytes.JPG'
-// import { useRef,useState } from 'react';
 
 export default function NotFound() {
-    
-
-
 
     return (
 
-        <div
-        className={cl.wrapper}
-        >
+        <div className={cl.wrapper}>
             <div className={cl.svgBg}>
                 <img 
                     src={svg} 
@@ -23,7 +15,7 @@ export default function NotFound() {
                 </div>
 
             <div className={cl.redirectTo}>
-               <p> Page not found. Go to <Link to='/'>home page</Link> </p>
+               <p>Page not found. Go to <Link to='/'> home page</Link> </p>
             </div>
             
         </div>
